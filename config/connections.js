634c5143a08e38fc8f0cf056b410301a5f46fe91
Barthélémy Jamois <barthelemy.jamois@creaktive.fr>
2015-07-30 09:48:45 -0500

    la même

--- config/connections.js
+++ config/connections.js
@@ -68,7 +68,7 @@ mongo_local: {
 
 mongo_bluemix:{
   adapter: 'sails-mongo',
-  url : process.env.VCAP_SERVICES ? JSON.parse(process.env.VCAP_SERVICES)["mongolab"][O].credentials.uri :'mongodb://localhost:27017/platform_creaktive'
+  url : process.env.VCAP_SERVICES ? JSON.parse(process.env.VCAP_SERVICES)["mongolab"].credentials.uri :'mongodb://localhost:27017/platform_creaktive'
 }
 
   /***************************************************************************
