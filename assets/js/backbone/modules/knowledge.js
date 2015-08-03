865b73322f173a34f0e48f6d092bd668386c90b2
J333P <jnpr.lh@gmail.com>
2015-08-03 11:04:43 +0200

    avancement knowledge 1.4

--- assets/js/backbone/modules/knowledge.js
+++ assets/js/backbone/modules/knowledge.js
@@ -17,36 +17,82 @@ var knowledge={
 
 
   init: function (json) {
-    if (knowledge.views.main == undefined){
-      this.views.main = new this.Views.Main({
+    if (knowledge.views.contributions == undefined){
+      this.views.contributions = new this.Views.Main({
         el : json.el,
         users : json.Users,
         contributions : json.Contributions
       });
     }
-    this.views.main.render()
+    this.views.contributions.render()
   }
 
 };
 
+///////////////////////////////////
+//CONTRIBUTIONS
+///////////////////////////////////
 
-///////////////////////////////////////
-//MAIN
-///////////////////////////////////////
-knowledge.Views.Main = Backbone.View.extend({
-  initialize: function(JSON){
+
+knowledge.Views.Contributions = Backbone.View.extend({
+
+  initialize: function(json){
     _.bindAll(this, "render");
-      el = this.el;
-      users = this.Users;
-    this.contribution = JSON.contribution ;
+    this.el = json.el;
+    this.contributions = json.contributions ;
 
     this.template = JST["assets/templates/knowledge_template.html"];
-    this.comments.on("add",this.render,this);
-    this.comments.on("remove",this.render,this);
+    this.contributions.on("add",this.render,this);
+    this.contributions.on("remove",this.render,this);
   },
 
   events : {
-    "click .addContributionComment": 'addComment'
+    "click .addContribution": 'addContribution',
+    "click .addContributionComment": 'addContributionComment'
+  },
+
+  addContribution: function(e){
+    e.preventDefault();
+    var contributionTextField = $("#contributionTextField").val();
+    this.contributions.create({
+      id_projet : this.id_projet,
+      id_phase : this id_phase,
+      content: contributionTextField
+    });
+  },
+
+  render: function(){
+    $(this.el).empty();
+    //this.id_contribution = id;
+
+
+    var posts = _.where(this.collectionPost.toJSON(),{id_post:id});
+
+    $(this.el).append(this.template({channel:id,posts:posts}));
+
+    return this;
+  },
+
+
+
+///////////////////////////////////////
+//ContributionComments
+///////////////////////////////////////
+
+
+knowledge.Views.ContributionComments = Backbone.View.extend({
+  el: "#containerComment",
+  initialize: function(JSON){
+    _.bindAll(this, "render");
+    this.collectionComment = JSON.collectionComment ;
+
+    this.template = JST["assets/templates/post_template.html"];
+    this.collectionComment.on("add",this.render,this);
+    this.collectionComment.on("remove",this.render,this);
+  },
+
+  events : {
+    "click .addComment": 'addComment'
   },
 
   addComment: function(e){
@@ -68,7 +114,6 @@ knowledge.Views.Main = Backbone.View.extend({
     $(this.el).append(this.template({post:id, comments:comments}));
 
     return this;
-  }
+  },
 
 
-});
